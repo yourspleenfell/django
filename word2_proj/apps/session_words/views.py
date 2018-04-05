34a18b82993225a6606560e8a8cc80e961c56cf1
Dylan <darbuthn@amazon.com>
2018-04-05 15:20:57 -0700

    updated word2_proj

--- word2_proj/apps/session_words/views.py
+++ word2_proj/apps/session_words/views.py
@@ -7,23 +7,24 @@ from time import gmtime, strftime
 
 # Create your views here.
 def index(request):
+    context = {
+        'time': strftime("%H:%M:%S %p, %B %d %Y")
+    }
     if 'list' not in request.session:
         request.session['list'] = []
-    print request.session['list']
-    return render(request, 'session_words/index.html')
+    return render(request, 'session_words/index.html', context)
 
 def submit(request):
-    if 'big' not in request.POST:
-        big = ""
-    else:
-        big = request.POST['big']
-    if 'color' not in request.POST:
-        color = ""
-    else:
-        color = request.POST['color']
-    request.session['color'] = ''
-    request.session['list'].append([request.POST['word'], color, big, strftime("%H:%M:%S %p, %B %d %Y")])
-    print request.session['list']
+    # if 'big' not in request.POST:
+    #     big = ""
+    # else:
+    #     big = request.POST['big']
+    # if 'color' not in request.POST:
+    #     color = ""
+    # else:
+    #     color = request.POST['color']
+    request.session['list'].append(request.POST)
+    request.session.modified = True
     return redirect('/')
 
 def clear(request):
